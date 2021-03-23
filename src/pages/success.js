97d85e89b207fcede76815e7c5335275bc0dab92
paulhabeeb <paulhabeeb@hey.com>
2021-03-23 14:27:34 -0400

    Add success page

---
+++ src/pages/success.js
@@ -0,0 +1,45 @@
+import React from 'react'
+import { graphql } from 'gatsby'
+
+import { Layout, PageHeader } from '@components/common'
+import * as styles from './Success.module.scss'
+
+export default function Success({ data }) {
+    const pageData = data.allPrismicTopFiveShoes.edges[0].node.data
+
+    return (
+        <main>
+            <Layout>
+                <PageHeader
+                    title={pageData.success_title.raw}
+                    description={pageData.success_description.raw}
+                    alignCenter={true}
+                />
+                <a href={pageData.success_link.url} className={styles.shop}>{pageData.success_link_text}</a>
+            </Layout>
+        </main>
+    )
+}
+
+export const query = graphql`
+    query SuccessQuery {
+        allPrismicTopFiveShoes(filter: {uid: {eq: "top-five-march-2021"}}) {
+            edges {
+                node {
+                    data {
+                        success_title {
+                            raw
+                        }
+                        success_description {
+                            raw
+                        }
+                        success_link_text
+                        success_link {
+                            url
+                        }
+                    }
+                }
+            }
+        }
+    }
+`
